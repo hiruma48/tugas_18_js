81b276ad7abb58621128612e9aae419808ecdd37
tri ariyanto <triyanto129@gmail.com>
2023-05-18 17:25:46 +0700

    tugas 18 js
    mengumpulkan tugas

---
+++ tugas18.js/tugas18_1.js
@@ -0,0 +1,5 @@
+function tugas18(){
+    var aku = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+    console.log(aku.slice(8,24));
+}
+tugas18();
